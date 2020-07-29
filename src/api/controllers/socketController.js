9e58495b2c046c8c913bc1306b96c873e1b5ccbd
Ludovic Jaffrezou <jaf.ludo@hotmail.fr>
2020-07-29 13:45:28 +0200

    broadcastjoinleft

--- src/api/controllers/socketController.js
+++ src/api/controllers/socketController.js
@@ -1,13 +1,18 @@
 exports.initserver = (io) =>{
 
-    //Session connexion client
+    //io -> server qui emet // socket -> socket qui a appele le serveur
     io.sockets.on('connection', function (socket) {
         console.log("client connected !")
 
         socket.on('disconnecting', (reason) => {
             console.log("client disconnected for " + reason + " !")
+            socket.broadcast.emit('userdisconnected',"anonymous left the tchat !\n");
         });
 
+        socket.on('messageget', (data) => {
+            socket.broadcast.emit('messageget',data);
+        });
+        
     });
 
 }
