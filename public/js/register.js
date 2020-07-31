8fc3d6ce1d1a9ee7b2ea5a8afc7c0231baefca53
Ludovic Jaffrezou <jaf.ludo@hotmail.fr>
2020-07-31 15:35:04 +0200

    fix

---
+++ public/js/register.js
@@ -0,0 +1,41 @@
+$("#form-register").submit(function(event) {
+
+    //Register can be execute all fields are validate !
+    if($("#register-pseudo").hasClass("alert-validate") 
+    || $("#register-email").hasClass("alert-validate") 
+    || $("#register-pass").hasClass("alert-validate")){
+        event.preventDefault();
+        console.log("not good !")
+    }else{
+        var pseudo = $("#register-pseudo-input").val();
+        var email = $("#register-email-input").val();
+        var password = $("#register-pass-input").val();
+        console.log("good !")
+        
+        $.ajax({
+            url : 'http://127.0.0.1:8080/users/register', // La ressource ciblée
+            type : 'POST', // Le type de la requête HTTP.
+
+            data : {
+                pseudo: pseudo,
+                email: email,
+                password: password
+            },
+
+            success: function(data){
+                //redirect index.js
+            },
+            error: function(e) {
+                console.log(e);
+
+            }
+        });
+        
+       //Cancel form redirect
+       event.preventDefault();
+
+       //Custom redirect
+       //window.location.href = "index.html";
+    }
+    
+});
